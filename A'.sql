054ffcc349c72419ccde2dfa18150c0f264e4cc0
Papenkov <64463542+Papenkov@users.noreply.github.com>
2021-08-13 17:41:45 +0300

    Update A'.sql

--- A'.sql
+++ A'.sql
@@ -1,5 +1,6 @@
-select AVG(price) from Purchases
-	join items on Purchases.itemid = items.itemid
-	join users on users.userID = Purchases.userid
-		where users.age between 18 and 25
+SELECT AVG(price) FROM Purchases
+	JOIN Items ON Purchases.itemid = Items.itemid
+	JOIN Users ON Users.userID = Purchases.userid
+		WHERE Users.age BETWEEN 18 AND 25
 
+# оператор BETWEEN включает в выборку крайние значения
