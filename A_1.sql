be158d7dd45c4ba184dbe813d300b113a6103c33
Papenkov <64463542+Papenkov@users.noreply.github.com>
2021-08-13 17:44:42 +0300

    Update A_1.sql

--- A_1.sql
+++ A_1.sql
@@ -1,6 +1,7 @@
-select DISTINCT strftime('%m', date) as month_number, 
-	avg(Items.price) over (partition by strftime('%m', date)) as avg_spend  from Purchases
-	join items on Purchases.itemid = items.itemid
-	join users on users.userID = Purchases.userid
-		where users.age between 18 and 25
-
+SELECT DISTINCT strftime('%m', date) AS month_number, 
+	AVG(Items.price) OVER (PARTITION BY strftime('%m', date)) AS avg_spend  FROM Purchases
+	JOIN Items ON Purchases.itemid = Items.itemid
+	JOIN Users ON Users.userID = Purchases.userid
+		WHERE Users.age BETWEEN 18 AND 25
+		
+# выдает результат средних трат по месяцам
