1b2724034b9a954ac8e696803fc1244dd76464b7
Papenkov <64463542+Papenkov@users.noreply.github.com>
2021-08-27 14:50:59 +0300

    Delete A'.sql

--- A'.sql
+++
@@ -1,6 +0,0 @@
-SELECT AVG(price) FROM Purchases
-	JOIN Items ON Purchases.itemid = Items.itemid
-	JOIN Users ON Users.userID = Purchases.userid
-		WHERE Users.age BETWEEN 18 AND 25
-
-# оператор BETWEEN включает в выборку крайние значения
